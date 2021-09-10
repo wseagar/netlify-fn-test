c42dc68a39b5b7dd8dfb0cd2572dea1d4103b589
Will Seagar <w.seagar@gmail.com>
2021-09-10 19:20:38 +1200

    add cors

--- netlify/functions/post.ts
+++ netlify/functions/post.ts
@@ -1,37 +1,48 @@
-import { Handler } from "@netlify/functions"
+import { Handler, HandlerEvent } from "@netlify/functions"
 import moment from "moment";
-
-const headers = {
-  "Content-Type": "application/json",
-  "Access-Control-Allow-Methods": "POST",
-}
 
 type Request = {
   location_ext_ids: string[];
 }
 
-const err = (message) => ({ statusCode: 400, body: JSON.stringify({ success: false, message }) });
-const ok = (payload) => ({ statusCode: 400, body: JSON.stringify({ success: true, data: payload }) });
+const headers = (event: HandlerEvent) => {
+  let cors = "https://*--vaxxnz.netlify.app";
+  if (event.headers.origin === "https://vaxx.nz") {
+    cors = "https://vaxx.nz";
+  }
+
+  return {
+    "Access-Control-Allow-Methods": "POST",
+    "Access-Control-Allow-Origin": cors,
+    "Content-Type": "application/json",
+  }
+}
+
+const err = (event, message) => ({ statusCode: 400, body: JSON.stringify({ success: false, message, headers: headers(event) }) });
+const ok = (event, payload) => ({ statusCode: 400, body: JSON.stringify({ success: true, data: payload, headers: headers(event) }) });
 
 const handler: Handler = async (event, context) => {
   const body = JSON.parse(event.body) as Request;
   const locationIds = body.location_ext_ids;
   const [dateString] = event.path.split("/").slice(-1);
   if (!dateString) {
-    return err("Invalid date");
+    return err(event, "Invalid date");
   }
   const date = moment(dateString, "YYYY-MM-DD");
   if (!date.isValid()) {
-    return err("Invalid date");
+    return err(event, "Invalid date");
   }
   if (!locationIds || !Array.isArray(locationIds)) {
-    return err("Invalid location_ext_ids");
+    return err(event, "Invalid location_ext_ids");
   }
   if (!locationIds.length) {
-    return err("Invalid location_ext_ids");
+    return err(event, "Invalid location_ext_ids");
   }
-  if (!locationIds.some((loc) => typeof loc === "string")){
-    return err("Invalid location_ext_ids");
+  if (!locationIds.some((loc) => typeof loc === "string")) {
+    return err(event, "Invalid location_ext_ids");
+  }
+  if (!process.env.PROXY_URL) {
+    return err(event, "Env variables not configured correctly");
   }
 
   const urls = locationIds.map(
